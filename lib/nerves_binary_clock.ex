e8bd0ae2d6b3ae0b41a98dc42cee30aa4a38c707
Masatoshi N <nishiguchi.masa@gmail.com>
2022-02-16 19:15:12 -0500

    Init with a new nerves project

---
+++ lib/nerves_binary_clock.ex
@@ -0,0 +1,18 @@
+defmodule NervesBinaryClock do
+  @moduledoc """
+  Documentation for NervesBinaryClock.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesBinaryClock.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_binary_clock_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesBinaryClockTest do
+  use ExUnit.Case
+  doctest NervesBinaryClock
+
+  test "greets the world" do
+    assert NervesBinaryClock.hello() == :world
+  end
+end
